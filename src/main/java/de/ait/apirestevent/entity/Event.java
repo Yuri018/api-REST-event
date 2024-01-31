273947264b043296e08c41c29a64cd108b98fa7a
Juri Ryzhkov <plktmn@gmail.com>
2024-01-31 09:07:56 +0100

    update

--- src/main/java/de/ait/apirestevent/entity/Event.java
+++ src/main/java/de/ait/apirestevent/entity/Event.java
@@ -1,2 +1,17 @@
-package de.ait.apirestevent.entity;public class Event {
+package de.ait.apirestevent.entity;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class Event {
+    private String name;
+    private String description;
+    private LocalDate date;
 }
